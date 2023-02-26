2e97839bf1a018c75afc796d44fc5bd6c5d41531
gaurav jadhav <gauravjadhav7512@gmail.com>
2023-02-26 18:14:25 +0530

    converting sentence to array of words in js :)

---
+++ Languages/Javascript/sentenceToWords.js
@@ -0,0 +1,6 @@
+const sentence ="hello, how are you doing?.."
+
+console.log(sentence.split(' '))
+console.log(sentence.split(' ')[0])
+console.log(sentence.split(' ')[1])
+console.log(sentence.split(' ')[2])
